1342f50a29d16a070f98dbb8fd2b98e2a2b5b8e6
Ceejay-777 <covenantcrackslord01@gmail.com>
2023-09-03 18:21:38 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,10 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-
   return (
-    <>
-      
-       
-    </>
+    <div className='w-full bg-slate-500'>
+       <h1>Open</h1>
+    </div>
   )
 }
 
