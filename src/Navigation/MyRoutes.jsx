0339b3345211b35cf28d4be1a071e25927587c58
Ceejay-777 <covenantcrackslord01@gmail.com>
2023-09-06 14:52:02 +0100

    comit

--- src/Navigation/MyRoutes.jsx
+++ src/Navigation/MyRoutes.jsx
@@ -5,15 +5,18 @@ import Page1 from "../Pages/Page1";
 import Page2 from "../Pages/Page2";
 import Page3 from "../Pages/Page3";
 import Page4 from "../Pages/Page4";
+import SharedLayout from "./SharedLayout";
 
 const MyRoutes = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Page1 />}></Route>
-        <Route path="Page2" element={<Page2 />}></Route>
-        <Route path="Page3" element={<Page3 />}></Route>
-        <Route path="Page4" element={<Page4 />}></Route>
+        <Route path="/" element={<SharedLayout />}>
+          <Route index element={<Page1 />}></Route>
+          <Route path="Page2" element={<Page2 />} />
+          <Route path="Page3" element={<Page3 />} />
+          <Route path="Page4" element={<Page4 />} />
+        </Route>
       </Routes>
     </Router>
   );
