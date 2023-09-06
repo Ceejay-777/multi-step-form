3830fa8f449fbacba041988d9dddaecbbfc99c02
Ceejay-777 <covenantcrackslord01@gmail.com>
2023-09-06 14:06:02 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="w-full bg-slate-500">
-      <h1>Open</h1>
+    <div className="w-full bg-slate-500 h-screen">
+      <h1>Ope</h1>
     </div>
   );
 }
