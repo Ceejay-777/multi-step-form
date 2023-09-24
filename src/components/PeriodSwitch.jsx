172e1b7c309b2184a98c3616f6f07d7afa0e4441
Ceejay-777 <covenantcrackslord01@gmail.com>
2023-09-24 12:56:54 +0100

    Period toggle

--- src/components/PeriodSwitch.jsx
+++ src/components/PeriodSwitch.jsx
@@ -3,15 +3,22 @@ import { usePeriodContext } from "../context/periodContext";
 
 const PeriodSwitch = () => {
   const [period, setPeriod] = usePeriodContext();
-  useEffect(() => {
-    console.log(period, setPeriod);
-  }, []);
+
+  const translation = period ? "left-0" : "left-6";
+
   return (
     <div className="flex gap-6 w-full py-4 border-2 rounded-sm bg-coolGray justify-center">
       <div>Monthly</div>
       <div>
-        <div className="w-12 h-6 rounded-full flex align-middle p-1 bg-marineBlue">
-          <div className="w-4 h-4 bg-white rounded-full"></div>
+        <div
+          className="w-12 h-6 rounded-full flex align-middle p-1 bg-marineBlue"
+          onClick={() => {
+            setPeriod(!period);
+          }}
+        >
+          <div
+            className={`w-4 h-4 bg-white rounded-full relative ${translation}`}
+          ></div>
         </div>
       </div>
       <div>Yearly</div>
--- src/context/periodContext.jsx
+++ src/context/periodContext.jsx
@@ -3,7 +3,7 @@ import { useContext, useState, createContext } from "react";
 const periodContext = createContext();
 
 export const Period = ({ children }) => {
-  const [period, setPeriod] = useState("monthly");
+  const [period, setPeriod] = useState(true);
 
   return (
     <periodContext.Provider value={[period, setPeriod]}>
