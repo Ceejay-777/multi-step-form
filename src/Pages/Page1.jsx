04e2e40016fdaeea06d78e5149fb6974fdd4752b
Ceejay-777 <covenantcrackslord01@gmail.com>
2023-09-24 23:12:00 +0100

    Period toggle

--- src/Pages/Page1.jsx
+++ src/Pages/Page1.jsx
@@ -1,10 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import PeriodSwitch from "../components/PeriodSwitch";
 
 const Page1 = () => {
+  const [namefield, setNamefield] = useState({ value: "", isError: false });
+  const [emailfield, setEmailfield] = useState({ value: "", isError: false });
+  const [phonefield, setPhonefield] = useState({ value: "", isError: false });
+
+  const handleInputChange = (event) => {
+    event.preventDefault();
+  };
   return (
     <div>
-      <PeriodSwitch />
+      <h1>Personal info</h1>
+      <p>Please provide your name, email address and phone number</p>
+
+      <div>
+        <label htmlFor="name">Name</label>
+        <br />
+        <input
+          type="text"
+          name="name"
+          id="name"
+          placeholder="e.g Stephen King"
+          className="w-full border-2"
+          value={namefield.value}
+        />
+      </div>
+
+      <div>
+        <label htmlFor="email">Email Address</label>
+        <br />
+        <input
+          type="email"
+          name="email"
+          id="email"
+          placeholder="e.g. stephenking@lorem.com"
+          className="w-full border-2"
+          value={emailfield.value}
+        />
+      </div>
+
+      <div>
+        <label htmlFor="name">Phone Number</label>
+        <br />
+        <input
+          type="tel"
+          name="phone"
+          id="phone"
+          placeholder="e.g. +1 234 567 890"
+          className="w-full border-2"
+          value={phonefield.value}
+        />
+      </div>
     </div>
   );
 };
