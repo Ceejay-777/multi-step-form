fd377a23df41f52429a2bb23167a91d48fd4f4f9
Ceejay-777 <covenantcrackslord01@gmail.com>
2023-09-30 23:09:45 +0100

    commit

--- src/Pages/Page3.jsx
+++ src/Pages/Page3.jsx
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import AddonBox from "../components/AddonBox";
 import { addons } from "../data";
 import { usePeriodContext } from "../context/periodContext";
 import PeriodSwitch from "../components/PeriodSwitch";
+import check from "../../assets/images/icon-checkmark.svg";
 
 const Page3 = ({ page3Vals, setPage3Vals }) => {
   const [monthly, setMonthly] = usePeriodContext();
+  const [selection, setSelecton] = useState([]);
 
-  use;
-
+  // useEffect(())
+  const handleSelection = (event) => {
+    console.log(event.target.clicked);
+    // if (!event.target.clicked) {
+    // }
+  };
   return (
     <form>
       {addons.map((singleAddon) => {
@@ -19,10 +25,13 @@ const Page3 = ({ page3Vals, setPage3Vals }) => {
             perk={perk}
             price={monthly ? `$${monthlyPrice}/mo` : `$${yearlyPrice}/yr`}
           >
-            <input type="checkbox" name="addon" id={addon} />
+            <div className="bg-purpleBlue p-1 border-2 w-4 aspect-square box-content">
+              <img src={check} className="w-full" />
+            </div>
           </AddonBox>
         );
       })}
+      <button type="submit">Okay</button>
       <PeriodSwitch />
     </form>
   );
