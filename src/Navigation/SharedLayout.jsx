07fd7c84d2516415f20ee1593483121b29fa724a
Ceejay-777 <covenantcrackslord01@gmail.com>
2024-01-05 14:10:29 +0100

    commit

--- src/Navigation/SharedLayout.jsx
+++ src/Navigation/SharedLayout.jsx
@@ -5,13 +5,15 @@ import Navbar from "./Navbar";
 const SharedLayout = () => {
   return (
     <div>
-      <div className="bg-[url('../../assets/images/bg-sidebar-mobile.svg')] bg-cover bg-no-repeat h-[172px]">
-        <div className="flex justify-center mb-5">
-          <Navbar />
-        </div>
-      </div>
       <div>
-        <Outlet />
+        <div className="bg-[url('../../assets/images/bg-sidebar-mobile.svg')] bg-cover bg-no-repeat h-[172px]">
+          <div className="flex justify-center mb-5">
+            <Navbar />
+          </div>
+        </div>
+        <div>
+          <Outlet />
+        </div>
       </div>
     </div>
   );
--- src/components/PageContainer.jsx
+++ src/components/PageContainer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const PageContainer = ({ children }) => {
   return (
-    <div className="bg-white w-11/12 mx-auto rounded-lg p-7 border-2 translate-y-[-60px] mb-[60px]">
+    <div className="bg-white w-11/12 mx-auto rounded-lg p-7 translate-y-[-60px] mb-[60px]">
       {children}
     </div>
   );
